2a4acbc639a3710a78e02d6f415d645793fb9117
JulienVERREC <julien.verrecchia@orange.fr>
2017-03-10 14:54:58 +0100

    problème résolu

--- js/app.js
+++ js/app.js
@@ -1,40 +1,41 @@
-var a = 10
-var b = 5
-
 //addition
 
 function addition(a, b) {
+	console.log(a + b);
 	return a + b;
 }
-console.log(a + b);
-
 //soustraction
 
 function soustraction(a, b){
+	console.log(a - b);
 	return a - b;
 }
-console.log(a - b);
-
 //multiplication
 
 function multiplication(a, b){
+	console.log(a * b);
 	return a * b;
 }
-console.log(a * b);
-
 //division
 
 function division(a, b){
-	return a / b;
-//si b>0 console.log resultat de la division a/b
-
-	if (b > 0) {
-		console.log(a / b);
+	
+	if (b > 0){ 
+	console.log(a / b);
 	}
-//si b=<0 console.log message erreur can't divide by 0
-
-	else (b =< 0) {
-		console.log("can't divide by 0");
+    
+	else { 
+	console.log("can't divide by 0");
+	return a / b;
 	}
 }
-console.log(a / b);
+//test addition
+        addition(5, 9)
+//test soustraction
+		soustraction(8, 3)
+//test multiplication
+        multiplication(3, 9)
+//test division
+        division(45, 5)
+//test division par 0
+        division(6, 0)        
